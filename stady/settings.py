2efb313223aedf7ac03c35b7922628eb636abceb
minsu <splendent77@gmail.com>
2022-10-20 00:19:52 +0900

    Update & Design : 회원탈퇴, 회원정보 변경, 회원비밀번호 변경, hover적용, input UI 정리

--- stady/settings.py
+++ stady/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'splendent77@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'fratzykgkoybokso'
 EMAIL_USE_TLS = True
