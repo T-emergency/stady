4587c227528dd237395ce3294a3f45b267e4b7d3
minsu <splendent77@gmail.com>
2022-10-23 16:11:21 +0900

    Update : community pagination

--- community/models.py
+++ community/models.py
@@ -8,10 +8,10 @@ from user.models import User
 class Post(models.Model):
     class Meta:
         db_table = 'community_post'
-
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    post_title = models.CharField(max_length=50)
-    post_content = models.CharField(max_length=200)
+    
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=200)
     create_date = models.DateTimeField(blank = True)
     image = models.ImageField(upload_to='post' , blank = True)
 
@@ -20,7 +20,10 @@ class Post(models.Model):
 class PostComment(models.Model):
     class Meta:
         db_table = 'community_comment'
+
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete = models.CASCADE)
-    comment_content = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete = models.CASCADE) #하나의 post를 볼 수 있게 pk
+    content = models.CharField(max_length=100)
     create_date = models.DateTimeField(blank = True)
+
+    # 타이틀 content로 _ 다지우기
--- community/views.py
+++ community/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from user.models import User
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -16,8 +17,12 @@ def post(request):
         return render(request, 'community/community.html/')
 
 def post_index(request):
+    page= request.GET.get('page','1') #페이지를  나누어주는 과정 호출했을때 기준페이지 1 설정
     post_list=Post.objects.order_by('-create_date')
-    context={'post_list' : post_list}
+    
+    paginator = Paginator(post_list, 10)
+    page_obj = paginator.get_page(page)
+    context={'post_list' : page_obj}
     return render(request, 'community/community.html', context)
 
 def post_detail(request, post_id):
