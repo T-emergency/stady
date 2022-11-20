b5b8fb73927c0b2df2d24a888fcebb08961c76d0
Kim GyeongMin <gyeongmin951@naver.com>
2022-11-20 13:26:03 +0900

    Update : ModelViewSet에 의한 urls.py actions지정, router으로 변경 필요, views.py액션 설정 필요

--- study_group/urls.py
+++ study_group/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from . import recommend
+from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register('testviewset', views.PrivateStudyCommentView)
 
 app_name = 'studies'
 
@@ -16,5 +20,16 @@ urlpatterns = [
     path('recommand/', recommend.create_recommand_csv),
 
     #Only Student generic view section
-    path('<int:study_id>/post/', views.PrivateStudyDetailView.as_view(), name='only_student_detail'),
+    # path('<int:study_id>/private/', views.PrivateStudyListView.as_view(), name='only_student_detail'),
+    path('<int:study_id>/private/', views.PrivateStudyView.as_view(), name='private_study'),
+    path('<int:study_id>/private/<int:post_id>/', views.PrivateStudyDetailView.as_view(), name='private_study_detail'),
+    path('<int:study_id>/private/<int:post_id>/comment/', views.PrivateStudyCommentView.as_view({
+          'post' : 'create'
+     })),
+     #TODO view파일에서 직접 설정하기 질문 <int:comment_id>와 설정? 혹은 여기에서 퍼미션 설정 질문
+    path('<int:study_id>/private/<int:post_id>/comment/<int:comment_id>/', views.PrivateStudyCommentView.as_view({
+          'put' : 'update',
+          'delete' : 'destroy'
+    })),
+#     path('<int:study_id>/private/<int:post_id>/comment/<int:comment_id>', views.PrivateStudyDetailView.as_view(), name='private_study_detail'),
 ]
